c980361da0b42c5150cafa0608ee408371d6e865
Ashwin Ayyappasamy <129774231+ashwin-30@users.noreply.github.com>
2023-12-04 19:42:19 +0530

    Create Sort Array 8.c

---
+++ Sort Array 8.c
@@ -0,0 +1,97 @@
+/*
+
+Sort the array elements in descending order according to their frequency of occurrence
+
+Sample Input 0
+
+10
+3 3 2 2 2 5 5 5 8 7
+Sample Output 0
+
+2 2 2 5 5 5 3 3 7 8
+*/
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+void sortInArray(int n,int a[]);
+int minimum(int n,int a[]);
+int maximum(int n,int a[]);
+int main() {
+
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+    int n;
+    scanf("%d",&n);
+    int a[n];
+    for(int i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    sortInArray(n,a);
+    return 0;
+}
+int minimum(int n,int a[])
+{
+    int min=a[0];
+    for(int i=0;i<n;i++)
+    {
+        if(min>a[i])
+        {
+            min=a[i];
+            
+        }
+        
+    }
+    // printf("%d ",min);
+    return min;
+}
+
+int maximum(int n,int a[])
+{
+    int max=a[0];
+    for(int i=0;i<n;i++)
+    {
+        if(max<a[i])
+        {
+            max=a[i];
+            
+        }
+        
+    }
+    return max;
+}
+
+void sortInArray(int n,int a[])
+{
+    int max=maximum (n,a);
+    int min=minimum(n,a);
+    int k=max+1-min;
+    
+    int b[k];
+    for(int i=0;i<k;i++)
+    {
+        b[i]=0;
+    }
+    for(int m=0;m<n;m++)
+    {
+        int h=a[m];
+        b[h-min]++;
+    }
+    int maxi=maximum(k,b);
+    // assending order
+    while(maxi>0)
+    {
+    for(int j=0;j<k;j++)
+    {
+        if(b[j]==maxi)
+        {
+            for(int k=1;k<=maxi;k++)
+            {
+                printf("%d ",j+min);
+            }
+            b[j]=0;
+        }
+    }
+    maxi--;
+    }
+}
